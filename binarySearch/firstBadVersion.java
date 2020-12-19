10d5608dbd52597ebc8a2ff2c18eab3e11d88d72
Luofei Shi <shiluofei@gmail.com>
2020-12-19 13:00:34 -0500

    implemented firstBadVersion, leetcode 278

---
+++ binarySearch/firstBadVersion.java
@@ -0,0 +1,25 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if (n == 0)
+            return 0;
+        
+        // use a binary search to minimize the call to API
+        int start = 1, end = n;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        if (isBadVersion(start))
+            return start;
+        
+        return end;
+    }
+}
