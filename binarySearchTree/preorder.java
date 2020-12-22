699d6da432be6bd35fbe847dfaad5acbbff50669
Luofei Shi <shiluofei@gmail.com>
2020-12-22 02:29:34 -0500

    implemented binary tree paths

--- binarySearchTree/preorder.java
+++ binarySearchTree/preorder.java
@@ -1,4 +1,5 @@
 public class Solution {
+	// 1.递归的定义：返回以root为根的二叉树的preorder
 	public ArrayList<Integer> preorderTraversal(TreeNode root) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		traverse(root, result);
