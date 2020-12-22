da6de18a531c13cf82894488422e52bac29cb356
Luofei Shi <shiluofei@gmail.com>
2020-12-22 02:12:52 -0500

    implemented the divided and conquer & traverse mode of max depth

---
+++ binarySearchTree/maximumDepthOfBinaryTree.java
@@ -0,0 +1,22 @@
+// traverse mode
+public class Solution {
+	private int depth;
+
+	public int maxDepth(TreeNode root) {
+		depth = 0;
+		helper(root, 1);
+
+		return depth;
+	}
+
+	private void helper(TreeNode node, int curtDepth) {
+		if (node == null)
+			return;
+
+		if (curtDepth > depth)
+			depth = curtDepth;
+
+		helper(node.left, curtDepth + 1);
+		helper(node.right, curtDepth + 1);
+	}
+}
