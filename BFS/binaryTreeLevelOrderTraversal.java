fa4a529fe61a89c53e51af4eb8d16b92a10c85c9
luofeishi <shiluofei@gmail.com>
2020-12-25 00:04:52 -0500

    implemented level order traverse for Binary Tree

---
+++ BFS/binaryTreeLevelOrderTraversal.java
@@ -0,0 +1,34 @@
+class Solution {
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> results = new ArrayList<>();
+
+		if (root == null) {
+			return results;
+		}
+
+		// 1. create a queue and push the root in
+		Queue<TreeNode> qeuue = new LinkedList<>();
+		queue.offer(root);
+
+		// 2. create a while loop if the queue is not empty
+		while (!queue.isEmpty()) {
+			ArrayList<Integer> level = new ArrayList<>();
+			int size = queue.size();
+
+			for (int i = 0; i < size; i++) {
+				TreeNode node = queue.poll();
+				level.add(node.val);
+				if (node.left != null) {
+					queue.offer(node.left);
+				} 
+				if (node.right != null) {
+					queue.offer(node.right);
+				}
+			}
+
+			results.add(level);
+		}
+
+		return results;
+	}
+}
