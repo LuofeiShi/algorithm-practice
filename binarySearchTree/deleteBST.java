7352a95e6f49b5b52b88a6a4266a1899fbeb7cd8
luofeishi <shiluofei@gmail.com>
2020-12-24 22:54:12 -0500

    update method in delete bst

--- binarySearchTree/deleteBST.java
+++ binarySearchTree/deleteBST.java
@@ -56,3 +56,68 @@ class Solution {
         return root.val;
     }
 }
+
+public class Solution2 {
+    public TreeNode removeNode(TreeNode root, int value) {
+        TreeNode dummy = new TreeNode(0);
+        dummy.left = root;
+
+        TreNode parent = findNode(dummy, root, value);
+
+        TreeNode node;
+        if (parent.left != null && parent.left.val == val) {
+            node = parent.left;
+        } else if (parent.right != null && parent.right.val == value) {
+            node = parent.right;
+        } else {
+            return dummy.left;
+        }
+
+        deleteNode(parent, node);
+
+        return dummy.left;
+    }
+
+    private TreeNode findNode(TreeNode parent, TreeNode node, int value) {
+        if (node == null) {
+            return parent;
+        }
+
+        if (node.val == value) {
+            return parent;
+        }
+
+        if (value < node.val) {
+            return findNode(node, node.left, value);
+        } else {
+            return findNode(node, node.right, value);
+        }
+    }
+
+    private void deleteNode(TreeNode parent, TreeNode node) {
+        if (node.right == null) {
+            if (parent.left == node) {
+                parent.left = node.left;
+            } else {
+                parent.right = node.left;
+            }
+        } else {
+            TreeNode temp = node.right;
+            TreeNode father = node;
+
+            while (temp.left != null) {
+                father = temp;
+                temp = temp.left;
+            }
+
+            if (father.left == temp) {
+                father.left = temp.right;
+            } else {
+                parent.right = temp;
+            }
+        }
+
+        temp.left = node.left;
+        temp.right = node.right;
+    }
+}
