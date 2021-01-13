29ff790143727cdae9c96d289a0502e233271011
Luofei Shi <shiluofei@gmail.com>
2021-01-13 12:02:29 -0500

    implemented triangle count

---
+++ twoPointers/triangleCount.java
@@ -0,0 +1,20 @@
+public Solution {
+	public int triangleCount(int S[]) {
+		int left = 0, right = S.length - 1;
+		int ans = 0;
+		Arrays.sort(S);
+		for (int i = 0; i < S.length; i++) {
+			left = 0;
+			right = i - 1;
+			while (left < right) {
+				if (S[left] + S[right] > S[i]) {
+					ans = ans + (right - left);
+					right--;
+				} else {
+					left++;
+				}
+			}
+		}
+		return ans;
+	}
+}
