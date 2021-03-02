7182335f7c8317f5c5a415307fc6a624bb73148c
LuofeiShi <59696046+LuofeiShi@users.noreply.github.com>
2021-03-02 00:22:03 -0500

    Create leetcode35.java

---
+++ leetcode31-40/leetcode35.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length; // the place might locate on the right of the array
+        
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] >= target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        
+        return l;
+    }
+}
